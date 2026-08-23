08099b2a7282ac012bd50ed956695be7c1135827
yatsuna827 <yatsuna_827@yahoo.co.jp>
2026-08-24 04:36:54 +0900

    feat: Hitコマンドの待機のCPU占有率を改善する

--- ORCA.Core/Util.cs
+++ ORCA.Core/Util.cs
@@ -5,9 +5,17 @@ namespace ORCA.Core
 {
     static class UtilExtensions
     {
+        private const int SpinMargin = 20;
+
         internal static bool CancelableWait(this Stopwatch sw, int wait_ms, in CancellationToken token, bool withRestart = true)
         {
             if (withRestart) sw.Restart();
+
+            // 待機完了の手前までは大雑把に待機し、直前だけwhileループにして精度を保証する
+
+            var sleep = wait_ms - (int)sw.ElapsedMilliseconds - SpinMargin;
+            if (sleep > 0 && token.WaitHandle.WaitOne(sleep)) return true;
+
             while (sw.ElapsedMilliseconds < wait_ms) if (token.IsCancellationRequested) return true;
             return false;
         }
